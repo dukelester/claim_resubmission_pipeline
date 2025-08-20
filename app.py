52153d2c16c8712b49ab5226e723597ee7382ed7
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2025-08-21 02:20:12 +0300

    Fixed some bugs in the FastApi endpoint regarding the binary files and outputs.

--- app.py
+++ app.py
@@ -12,15 +12,15 @@ async def process_files(alpha: UploadFile = File(None),
     # Save uploaded files temporarily for processing
     if alpha:
         alpha_file_path = f"temp_{alpha.filename}"
-        with open(alpha_file_path, "wb", encoding="utf-8") as alpha_file:
-            alpha_file.write(await alpha_file.read())
+        with open(alpha_file_path, "wb") as alpha_file:
+            alpha_file.write(await alpha.read())
     else:
         alpha_file_path = "data/emr_alpha.csv"
 
     if beta:
         beta_file_path = f"temp_{beta.filename}"
-        with open(beta_file_path, "wb", encoding="utf-8") as beta_file:
-            beta_file.write(await beta_file.read())
+        with open(beta_file_path, "wb") as beta_file:
+            beta_file.write(await beta.read())
     else:
         beta_file_path = "data/emr_beta.json"
 
